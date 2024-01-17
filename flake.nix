792f63797b5370e2787de53bf10a7ab76bf55746
Alessandro Candido <candido.ale@gmail.com>
2024-01-17 23:38:57 +0100

    Add nix files

---
+++ flake.nix
@@ -0,0 +1,52 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    systems.url = "github:nix-systems/default";
+    devenv.url = "github:cachix/devenv";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    devenv,
+    systems,
+    ...
+  } @ inputs: let
+    forEachSystem = nixpkgs.lib.genAttrs (import systems);
+  in {
+    # packages = forEachSystem (system: {
+    #   default =
+    #     nixpkgs.legacyPackages.${system}.poetry2nix.mkPoetryApplication
+    #     {
+    #       projectDir = self;
+    #       preferWheels = true;
+    #     };
+    # });
+
+    devShells =
+      forEachSystem
+      (system: let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in {
+        default = devenv.lib.mkShell {
+          inherit inputs pkgs;
+          modules = [
+            {
+              languages.javascript = {
+                enable = true;
+                corepack.enable = true;
+              };
+              languages.typescript = {
+                enable = true;
+              };
+            }
+          ];
+        };
+      });
+  };
+
+  nixConfig = {
+    extra-trusted-public-keys = "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw=";
+    extra-substituters = "https://devenv.cachix.org";
+  };
+}
